a7f1d0d950d5d97348f7f4c19c4aa0348bab7422
EnzooKms <ekemmas@edenschool.fr>
2022-10-28 14:00:47 +0200

    v3

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
-const count = document.getElementById("seconds")
-let counter = 0
+const countSeconds = document.getElementById("seconds")
+const countMinutes = document.getElementById("minutes")
+const counHours = document.getElementById("hours")
+let counter = 1
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time
+}
 
 const timer = setInterval(() => {
+    const hours = Math.floor(counter / 3600)
+    const minutes = Math.floor(counter / 60) % 60
+    const seconds = Math.floor(counter % 60)
+
+    counHours.innerHTML = formatTime(hours)
+    countMinutes.innerHTML = formatTime(minutes)
+    countSeconds.innerHTML = formatTime(seconds)
+
     counter++
-    count.innerHTML = counter
+
 }, 1 * 1000)
